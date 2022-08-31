acdbf9b5b64267807cb75a70e9c3d12467f8cddd
clemilano <clemi.milacurb@gmail.com>
2022-08-31 18:04:06 -0300

    agregue el ejercicio de la entrega

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ for(let i = 1; i < 12; i = i + 1){
     console.log("Alfajor Portezuelo");
 }
 
+
+for(let i = 1; i <= 22; i++){
+    let ingresarCantidadDeAlfajores = prompt("Ingresar cantidad de alfajores")
+    if(ingresarCantidadDeAlfajores < 22){
+        break;
+    }
+    alert("Seleccionaste muchos alfajores pocho")
+}
+
+
