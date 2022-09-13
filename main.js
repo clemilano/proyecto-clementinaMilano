e5d12d7010ee8440ad79a65dd4693c43c2e2f726
clemilano <clemi.milacurb@gmail.com>
2022-09-13 09:52:17 -0300

    corregi algunos detalles

--- main.js
+++ main.js
@@ -1,79 +1,13 @@
-/*console.log("Alfajor Portezuelo:  ");
-
-for(let i = 1; i < 12; i = i + 1){
-    console.log("Alfajor Portezuelo");
-}
-
-
-for(let i = 1; i <= 22; i++){
-    let ingresarCantidadDeAlfajores = prompt("Ingresar cantidad de alfajores")
-    if(ingresarCantidadDeAlfajores < 22){
-        break;
-    }
-    alert("Seleccionaste muchos alfajores pocho")
-}
-*/
-
-/*const sumar = ( num1, num2 ) => num1 + num2;
-
-const restar = ( num1, num2 ) => num1 - num2;
-
-const multiplicar = ( num1, num2 ) => num1 * num2;
-
-const dividir = ( num1, num2) => num1 / num2;
-
-function mostrarValores ( operacion, num1, num2){
-    switch ( operacion ){
-        case"+":
-        console.log(sumar ( num1, num2 ))
-        break;
-        case"-":
-        console.log(restar (num1, num2))
-        break;
-        case"*":
-        console.log(multiplicar (num1, num2))
-        break;
-        case"/":
-        console.log(dividir (num1, num2))
-        break;
-        default:
-            console.log("Operation invalid")
-    }
-}
-
-let answer;
-do{
-    let num1, operacion, num2
-    do{
-        num1 = parseFloat(prompt("Enter the number"))
-        operacion = prompt("Enter the sing of operation (+,-,*,/)")
-        num2 = parseFloat(prompt("Enter the number"))
-
-        if(isNaN (num1) || isNaN(num2)){
-            console.log("The number entert is not valid")
-        }
-
-        if (num2 === 0 && operacion == "/"){
-            console.log("It is not possible to divide by zero")
-        }
-    }while((isNaN(num1) || isNaN(num2) || (num2 === 0 && operacion == "/")))
-    mostrarValores(operacion, num1, num2)
-    do{
-        answer = prompt("You want to perform another operation?").toLocaleLowerCase()
-    }while(answer != "yes" && answer != "not")
-}while(answer != "not")
-*/
-
 const arrayProduct = [
-    { nombre: "Dulce de leche dietetico", gramos: 500, marca: "Los nietitos", precio: 208 },
-    { nombre: "Dulce de batata sin azucar", gramos: 400, marca: "Los nietitos", precio: 69},
-    { nombre: "Mermelada de frutilla", gramos: 250, marca: "Los nietitos", precio: 78},
-    { nombre: "Dulce de membrillo", gramos: 400, marca: "Los nietitos", precio: 69},
-    { nombre: "Manteca Conaprole", gramos: 100, marca: "Conaprole", precio: 50},
-    { nombre: "Galletas Paninna", gramos: 500, marca: "El trigal", precio: 99},
-    { nombre: "Galletas Sin Azucar", gramos: 300, marca: "El trigal", precio: 105},
-    { nombre: "Galletas Salvado", gramos: 350, marca: "El trigal", precio: 119},
-    { nombre: "Salamin al vacio Centenario", gramos: 180, marca: "Centenario", precio: 135},
+    { nombre: "Dulce de leche dietetico", gramos: 500, marca: "Los nietitos", precio: 208, id: 1},
+    { nombre: "Dulce de batata sin azucar", gramos: 400, marca: "Los nietitos", precio: 69, id: 2},
+    { nombre: "Mermelada de frutilla", gramos: 250, marca: "Los nietitos", precio: 78, id: 3},
+    { nombre: "Dulce de membrillo", gramos: 400, marca: "Los nietitos", precio: 69, id: 4},
+    { nombre: "Manteca Conaprole", gramos: 100, marca: "Conaprole", precio: 50, id: 5},
+    { nombre: "Galletas Paninna", gramos: 500, marca: "El trigal", precio: 99, id: 6},
+    { nombre: "Galletas Sin Azucar", gramos: 300, marca: "El trigal", precio: 105, id: 7},
+    { nombre: "Galletas Salvado", gramos: 350, marca: "El trigal", precio: 119, id: 8},
+    { nombre: "Salamin al vacio Centenario", gramos: 180, marca: "Centenario", precio: 135, id: 9},
 ];
 let carrito = []
 
@@ -147,14 +81,17 @@ while(seleccion != "not"){
         while(seleccion === "not"){
             alert("Su producto llegara lo mas breve posible a su hogar. Desde ya muchas gracias, vuelva siempre.")
             break;
-        }
+        } 
+
         carrito.push({ arrayProduct, cantidad, precio})
         console.log(carrito)
+        console.log(precio)
+        console.log(id)
 
         carrito.forEach((carritoFinal) => {
-            console.log( `arrayProduct: ${carritoFinal.arrayproduct}, unidades: ${carritoFinal.cantidad}, Total a pagar $ {carritoFinal.cantidad * carritoFinal.precio}`)
+            console.log( `arrayProduct: ${carritoFinal.arrayProduct}, unidades: ${carritoFinal.cantidad}, Total a pagar ${carritoFinal.cantidad * carritoFinal.precio}`)
         })
-        break;
+        
     } else{
         alert("No hay en stock")
     }
@@ -162,3 +99,5 @@ while(seleccion != "not"){
 
 const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
 console.log("El costo total de su compra es: ${}")
+
+
